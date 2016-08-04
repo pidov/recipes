d00357bfc1c8315600f6c72c24bd9acb104ddb88
Ivan Pidov <ivan.pidov@tick42.com>
2016-08-04 18:07:31 +0300

    Added recursive navigation

--- src/components/MainNavigation.vue
+++ src/components/MainNavigation.vue
@@ -8,62 +8,11 @@
   <div id="cs-div-navigation">
     <nav id="cs-main-navigation" class="cs-clearfix">
       <ul class="cs-main-navigation cs-clearfix">
-        <li class="current-menu-item"><a href="#"><span>Homepages</span></a>
-          <ul class="sub-menu">
-            <li class="current-menu-item"><a href="index.html">Homepage</a></li>
-            <li><a href="index_1.html">Homepage 1</a></li>
-            <li><a href="index_2.html">Homepage 2</a></li>
-          </ul>
-        </li>
-        <li><a href="#"><span>Features</span></a>
-          <ul class="sub-menu">
-            <li><a href="#"><span>Page templates</span></a>
-              <ul class="sub-menu">
-                <li><a href="page_shortcodes.html">Shortcodes</a></li>
-                <li><a href="page_404.html">Page 404</a></li>
-                <li><a href="page_contact.html">Contact us</a></li>
-                <li><a href="page_construction.html">Under construction</a></li>
-                <li><a href="page_fullwidth.html">Page fullwidth</a></li>
-              </ul>
-            </li>
-            <li><a href="#"><span>Post templates</span></a>
-              <ul class="sub-menu">
-                <li><a href="post_standard.html">Standard post</a></li>
-                <li><a href="post_video.html">Video post</a></li>
-                <li><a href="post_audio.html">Audio post</a></li>
-                <li><a href="post_gallery.html">Gallery post</a></li>
-              </ul>
-            </li>
-            <li><a href="#"><span>Header styles</span></a>
-              <ul class="sub-menu">
-                <li><a href="header_style_1.html">Header style 1</a></li>
-                <li><a href="header_style_2.html">Header style 2</a></li>
-              </ul>
-            </li>
-            <li><a href="#"><span>Layout</span></a>
-              <ul class="sub-menu">
-                <li><a href="boxed_image.html">Boxed with image</a></li>
-                <li><a href="boxed_pattern.html">Boxed with pattern</a></li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-        <li><a href="#"><span>Recipe category</span></a>
-          <ul class="sub-menu">
-            <li><a href="recipe_category_layout_1.html">Recipe category 1</a></li>
-            <li><a href="recipe_category_layout_2.html">Recipe category 2</a></li>
-            <li><a href="recipe_category_layout_3.html">Recipe category 3</a></li>
-          </ul>
-        </li>
-        <li><a href="#"><span>Single recipe</span></a>
-          <ul class="sub-menu">
-            <li><a href="recipe_single_layout_1.html">Recipe layout #1 (gallery)</a></li>
-            <li><a href="recipe_single_layout_2.html">Recipe layout #2</a></li>
-            <li><a href="recipe_single_layout_3.html">Recipe layout #3</a></li>
-          </ul>
-        </li>
-        <li><a href="blog_category.html">Blog</a></li>
-        <li><a href="page_contact.html">Contact us</a></li>
+        <menu-item
+          v-for="item in menuItems"
+          :title="item.title"
+          :url="item.url"
+          :children="item.children"></menu-item>
       </ul>
     </nav>
   </div>
@@ -71,4 +20,40 @@
 </template>
 
 <script>
+import MenuItem from './MenuItem';
+
+export default {
+  data() {
+    return {
+      menuItems: [{
+        title: 'Homepages',
+        url: '#',
+        children: [{
+          title: 'Sub one',
+          url: '#SubOne',
+          children: [{
+            title: 'Going deep',
+            url: 'Deeper!',
+          }],
+        }, {
+          title: 'Sub Twone',
+          url: '#SubOne',
+        }],
+      }, {
+        title: 'Second Top',
+        url: 'SecondURL',
+        children: [{
+          title: 'Sub one',
+          url: '#SubOne',
+        }, {
+          title: 'Sub one',
+          url: '#SubOne',
+        }],
+      }],
+    };
+  },
+  components: {
+    MenuItem,
+  },
+};
 </script>
---
+++ src/components/MenuItem.vue
@@ -0,0 +1,21 @@
+<template>
+<li>
+  <a href="{{url}}">
+    <span>{{title}}</span>
+  </a>
+  <ul class="sub-menu" v-if="children">
+    <menu-item
+      v-for="child in children"
+      :title="child.title"
+      :url="child.url"
+      :children="child.children"></menu-item>
+  </ul>
+</li>
+
+</template>
+<script>
+export default {
+  name: 'menu-item',
+  props: ['title', 'url', 'children'],
+};
+</script>
