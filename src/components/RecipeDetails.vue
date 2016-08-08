f2424ee1e82c3d0198e5d963e76acfa39fb982a3
Ivan Pidov <ivan.pidov@gmail.com>
2016-08-08 19:08:43 +0300

    [Component Add]: RecipeDetails

---
+++ src/components/RecipeDetails.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="cs-recipe-details">
+    <div> v-for="detail in details"
+      <span>{{detail.title}}</span>
+      {{detail.value}}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['title', 'details'],
+  };
+</script>
