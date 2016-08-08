dee0c45f7e1e862555c65be0f9d0be588b91033c
Ivan Pidov <ivan.pidov@gmail.com>
2016-08-08 19:13:51 +0300

    [Component Add]: NutritionList

---
+++ src/components/NutritionList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="cs-nutrition-list">
+    <h4>{{title}}</h4>
+    <ul class="cs-clearfix">
+      <li v-for="item in items">
+          <div class="cs-nutrition-item-description">
+            {{item.description}}
+          </div>
+          <div class="cs-nutrition-item-value">
+            {{item.value}}
+          </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['title', 'items'],
+  };
+</script>
