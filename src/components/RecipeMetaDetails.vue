4b03c129b718d4b242bac770e2988cb6f70b58f9
Ivan Pidov <ivan.pidov@gmail.com>
2016-08-08 19:10:46 +0300

    [Component Add]: RecipeMetaDetails

---
+++ src/components/RecipeMetaDetails.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="cs-recipe-meta-details">
+    <div class="print">
+      <a href="javascript:window.print()">
+        <i class="fa fa-print"></i>
+        Print
+      </a>
+    </div>
+    <div class="views">
+      <a href="#">
+        <i class="fa fa-eye"></i>
+        {{views}}
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['views'],
+  };
+</script>
