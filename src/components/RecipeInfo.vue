733caf0fdaf1fcf74d2835e5f917cbd6795e62ab
Ivan Pidov <ivan.pidov@gmail.com>
2016-08-08 19:04:53 +0300

    [Component Add]: RecipeInfo

---
+++ src/components/RecipeInfo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="cs-recipe-info">
+    <h3>{{title}}</h3>
+    {{{body}}}
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['title', 'body'],
+  };
+</script>
